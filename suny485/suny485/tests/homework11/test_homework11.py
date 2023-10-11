e932137e1d3987131923c77f79a78d6ae09aad2e
Owen Cozine <ocozi001@plattsburgh.edu>
2023-10-11 16:17:39 -0400

    added homework12 and revised homework11 to be paramitized

--- suny485/suny485/tests/homework11/test_homework11.py
+++ suny485/suny485/tests/homework11/test_homework11.py
@@ -1,38 +1,44 @@
 import pytest
+from suny485.projects.homework11.fruit_formal import get_formal_name
 
-from suny485.projects.homework11.fruit_formal import get_formal_name 
-#(part2 of hw)I ran this code but changed homework11.fruit_formal to homework11.fruit_formal_improved and one of my tests did not pass
+# Parametrize the test for valid fruit names
+@pytest.mark.parametrize("fruit_name, formal_name", [
+    ('apple', 'Malus domestica'),
+    ('banana', 'Musa acuminata'),
+    ('orange', 'Citrus × sinensis'),
+    ('strawberry', 'Fragaria × ananassa'),
+    ('grape', 'Vitis vinifera'),
+    ('pineapple', 'Ananas comosus'),
+    ('mango', 'Mangifera indica'),
+    ('blueberry', 'Vaccinium corymbosum'),
+    ('peach', 'Prunus persica'),
+    ('watermelon', 'Citrullus lanatus'),
+    ('cherry', 'Prunus avium'),
+    ('pear', 'Pyrus'),
+    ('plum', 'Prunus domestica'),
+    ('raspberry', 'Rubus idaeus'),
+    ('kiwi', 'Actinidia deliciosa'),
+    ('lemon', 'Citrus limon'),
+    ('avocado', 'Persea americana'),
+    ('pomegranate', 'Punica granatum'),
+    ('cranberry', 'Vaccinium macrocarpon'),
+    ('grapefruit', 'Citrus × paradisi'),
 
-def test_valid_fruit():
-    #testing to make sure all these fruits are in the dictionary as well as their formal name adsa 
-    assert get_formal_name('apple') == 'Malus domestica'
-    assert get_formal_name('banana') == 'Musa acuminata'
-    assert get_formal_name('orange') == 'Citrus × sinensis'
-    assert get_formal_name('strawberry') == 'Fragaria × ananassa'
-    assert get_formal_name('grape') == 'Vitis vinifera'
-    assert get_formal_name('pineapple') == 'Ananas comosus'
-    assert get_formal_name('mango') == 'Mangifera indica'
-    assert get_formal_name('blueberry') == 'Vaccinium corymbosum'
-    assert get_formal_name('peach') == 'Prunus persica'
-    assert get_formal_name('watermelon') == 'Citrullus lanatus'
-    assert get_formal_name('cherry') == 'Prunus avium'
-    assert get_formal_name('pear') == 'Pyrus'
-    assert get_formal_name('plum') == 'Prunus domestica'
-    assert get_formal_name('raspberry') == 'Rubus idaeus'
-    assert get_formal_name('kiwi') == 'Actinidia deliciosa'
-    assert get_formal_name('lemon') == 'Citrus limon'
-    assert get_formal_name('avocado') == 'Persea americana'
-    assert get_formal_name('pomegranate') == 'Punica granatum'
-    assert get_formal_name('cranberry') == 'Vaccinium macrocarpon'
-    assert get_formal_name('grapefruit') == 'Citrus × paradisi'
+])
+def test_valid_fruit(fruit_name, formal_name):
+    assert get_formal_name(fruit_name) == formal_name
 
-def test_invalid_fruit():
-    #testidng to see possible inputs that do not exist in dictionary which will produce a 'Unknown'
-    assert get_formal_name('boba') == 'Unknown'
-    assert get_formal_name('cucumbers') == 'Unknown'
-    assert get_formal_name('durian') == 'Unknown'
-    assert get_formal_name('') == 'Unknown'
-    assert get_formal_name('!') == 'Unknown'
+# Parametrize the test for invalid fruit names
+@pytest.mark.parametrize("fruit_name", [
+    'boba',
+    'cucumbers',
+    'durian',
+    '',
+    '!',
+
+])
+def test_invalid_fruit(fruit_name):
+    assert get_formal_name(fruit_name) == 'Unknown'
 
 if __name__ == "__main__":
     pytest.main()
